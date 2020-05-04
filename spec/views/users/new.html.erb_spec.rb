5896aa926fb29ba280fda9bce94a17c948dd8631
tamtt-1031 <tran.thanh.tam@sun-asterisk.com>
2020-05-04 14:02:23 +0700

    Config docker basic

---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", type: :view do
+  before(:each) do
+    assign(:user, User.new(
+      :name => "MyString",
+      :is_sent_mail => 1
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input[name=?]", "user[name]"
+
+      assert_select "input[name=?]", "user[is_sent_mail]"
+    end
+  end
+end
